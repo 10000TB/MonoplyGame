fe500dbf79b2d88d95f1b5fe7db88dcfcc3e320e
Kv8281 <Kv8281@10.84.46.51>
2016-10-20 23:49:20 -0600

    More functions added in Property.java

--- src/Property.java
+++ src/Property.java
@@ -3,7 +3,7 @@ public class Property extends Tile{
 
    private int cost;       // cost to build one house
    private int rent;
-   private int houseCost;  // cost to purchase one house
+   private int purchasePrice;  // cost to purchase one house
    private int numOfHouses;
    private int numOfHotels;
    private String owner;
@@ -15,6 +15,36 @@ public class Property extends Tile{
       setDesc (desc);
       setType (type);
    
+   }
+   
+   
+   
+   public int getCost () {
+	   return cost;
+   }
+   
+   public int getRent () {
+	   return rent;
+   }
+   
+   public int getPurchasePrice () {
+	   return purchasePrice;
+   }
+   
+   public int getNumOfHouses () {
+	   return numOfHouses;
+   }
+   
+   public int getNumOfHotels () {
+	   return numOfHotels;
+   }
+   
+   public String getOwner () {
+	   return owner;
+   }
+   
+   public boolean morg () {
+	   return morgStatus;
    }
    
 
