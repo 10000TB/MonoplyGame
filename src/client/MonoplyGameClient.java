8432964dbc49be343ad4e3e5c4dde684f1a2ed84
10000TB <xuehaohu@rams.colostate.edu>
2016-11-17 14:31:28 -0700

    --- init rmi base

---
+++ src/client/MonoplyGameClient.java
@@ -0,0 +1,17 @@
+package client;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+
+//import common.*;
+
+public class MonoplyGameClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
